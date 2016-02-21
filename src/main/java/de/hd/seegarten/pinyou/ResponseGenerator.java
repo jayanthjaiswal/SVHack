bdca8431c2bbe66128ca2a68b0b87af126f83882
Pratik Kumar <pratik.k.kumar@oracle.com>
2016-02-21 02:30:02 -0800

    top 3

--- src/main/java/de/hd/seegarten/pinyou/ResponseGenerator.java
+++ src/main/java/de/hd/seegarten/pinyou/ResponseGenerator.java
@@ -3,14 +3,21 @@ package de.hd.seegarten.pinyou;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream.GetField;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONArray;
 
 public class ResponseGenerator {
   
   final String ACCESS_KEY = "MTQzMTU5NDo1MDI3MTQ1MTQ1NjM4MDIzODU6OTIyMzM3MjAzNjg1NDc3NTgwN3wxNDU2MDMwOTA1OjI1OTIwMDAtLWRjODEzNTJiZWMyOTlhZjgxNThmM2MxODgyMjY4MWQ4";
   final Integer PAGE_SIZE = 100;
-  public String kw_to_response(String[] kws) throws IOException {
+  public String kw_to_response(String[] kws) throws IOException, JSONException {
     StringBuilder sb = new StringBuilder(); // Using default 16 character size
     int i;
     for(i = 0; i < kws.length - 1; i++) {
@@ -29,11 +36,19 @@ public class ResponseGenerator {
        result.append(line);
     }
     rd.close();
-    return result.toString();
+    JSONObject obj = new JSONObject(result.toString());
+    JSONArray obj2 = (JSONArray) obj.get("data");
+    List<String> alist = new ArrayList<>();
+    for (int i1 = 0; i1 < 3; i1++) {
+      String x = (String)((JSONObject)obj2.get(i1)).get("id");
+      alist.add(x);
+    }
+    
+    return alist.toString();
   }
   
-  public static void main(String[] args) throws IOException {
-    String[] kws = {"test", "blood"};
+  public static void main(String[] args) throws IOException, JSONException {
+    String[] kws = {"forest", "gump"};
     ResponseGenerator rg = new ResponseGenerator();
     System.out.println(rg.kw_to_response(kws));
   }
